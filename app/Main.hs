f69bb512f1cecc714a1f04152b38d9bed29b165a
Benjamin Flin <ben@flin.org>
2020-12-03 14:35:34 -0800

    added readLines

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
 import Lib
+import qualified Data.ByteString as B
+import qualified Data.Vector as V
+import Data.Vector ((!))
 
 main :: IO ()
-main = someFunc
+main = do 
+   v <- readLines "res/words.txt"
+   print $ v!0 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    ( readLines
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.IO (withFile, IOMode(ReadMode), hIsEOF)
+import qualified Data.ByteString as B
+import qualified Data.Vector as V
+
+
+
+readLines :: String -> IO (V.Vector B.ByteString)
+readLines filename = withFile filename ReadMode ((V.fromList <$>) . getLines)
+    where
+    getLines handle = do
+        eof <- hIsEOF handle  
+        if eof then 
+            return [] 
+        else
+            (:) <$> B.hGetLine handle <*> getLines handle 
+
+
